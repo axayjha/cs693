96d0cc32aa0bbcb7713dd62742903052562f0330
axayjha <akshayjha@live.in>
2018-03-19 00:38:50 +0530

    fixed it

--- Day 4/9_base.sh
+++ Day 4/9_base.sh
@@ -1,20 +1,51 @@
 echo -n "Enter the number: "
 read n
+n=`echo "$n" | tr [:lower:] [:upper:]`
+
 echo -n "Enter input base: "
 read ib
+ib=`echo "$ib" | tr [:lower:] [:upper:]`
+if ! [[ $ib =~ ^-?[0-9]+$ ]];
+then
+	echo "Input base should be a valid base10 number"
+	exit;
+fi
+
+
 echo -n "Enter the output base: "
 read ob
-[ $ib -le 1 ] && echo "Input base cannot be less than 2" && exit;
-[ $ob -le 1 ] && echo "Output base cannot be less than 2" && exit;
+ob=`echo "$ob" | tr [:lower:] [:upper:]`
+if ! [[ $ob =~ ^-?[0-9]+$ ]];
+then
+	echo "Output base should be a valid base10 number"
+	exit;
+fi
+
+if [ $ib -le 1 ] || [ $ib -gt 16 ];
+then
+	echo "Input base out of bounds";
+	exit;
+fi
+if [ $ob -le 1 ] || [ $ob -gt 16 ];
+then
+	echo "Output base out of bounds";
+	exit;
+fi
 
 for (( i=0; i<${#n}; i++ ));
-do
-	if [ ${n:$i:1} -ge $ib ];
+do	
+ 	dig="${n:$i:1}";
+	dig="obase="10"; ibase="$ib"; $dig"
+	dig=`echo $dig|bc`
+	if [ $dig -ge $ib ];
 	then
 		echo "Input has a digit greater than base";
 		exit;
 	fi
 done
+
+
+
 
 a="obase="$ob"; ibase="$ib"; $n"
 echo $a|bc
