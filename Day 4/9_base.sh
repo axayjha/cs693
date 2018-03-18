3b41f13549c49edd3f22935430f15ef03d70df60
axayjha <akshayjha@live.in>
2018-03-18 23:46:12 +0530

    fixed base conv

--- Day 4/9_base.sh
+++ Day 4/9_base.sh
@@ -4,5 +4,17 @@ echo -n "Enter input base: "
 read ib
 echo -n "Enter the output base: "
 read ob
+[ $ib -le 1 ] && echo "Input base cannot be less than 2" && exit;
+[ $ob -le 1 ] && echo "Output base cannot be less than 2" && exit;
+
+for (( i=0; i<${#n}; i++ ));
+do
+	if [ ${n:$i:1} -ge $ib ];
+	then
+		echo "Input has a digit greater than base";
+		exit;
+	fi
+done
+
 a="obase="$ob"; ibase="$ib"; $n"
 echo $a|bc
