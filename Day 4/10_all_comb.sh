e1b80c4853e2f8587244137bddcd6c30fdfdcc67
axayjha <akshayjha@live.in>
2018-05-08 14:41:39 +0530

    fixed all combinations

--- Day 4/10_all_comb.sh
+++ Day 4/10_all_comb.sh
@@ -1,3 +1,16 @@
+perm() {
+  local items="$1"
+  local out="$2"
+  local i
+
+  [[ "$items" == "" ]] && echo "$out" && return
+
+  for (( i=0; i<${#items}; i++ )) ; do
+    perm "${items:0:i}${items:i+1}" "$out${items:i:1}"
+  done
+  }
+  
 echo -n "Enter a string: "
 read str
-
+echo "Permutations: "
+perm $str
